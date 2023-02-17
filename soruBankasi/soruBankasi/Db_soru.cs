39467fdc0e7ec00fce02b9e353ca90296e9e4064
f6c5 <cicekf256@gmail.com>
2023-02-17 17:48:45 +0300

    ...

--- soruBankasi/soruBankasi/Db_soru.cs
+++ soruBankasi/soruBankasi/Db_soru.cs
@@ -1,13 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace soruBankasi
 {
     internal class Db_soru
     {
+        public void basla()
+        {
+            string Url = " datasource=localhost;port=3306;username=root;password=1234;database=db_soru";
+           
+
+        }
+
 
     }
 }
